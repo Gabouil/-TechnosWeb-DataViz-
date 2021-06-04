0c4fc5ba22e024a12eeb02834f8f24ca3703f9a1
Gabouil <gabriel.goldbronnn@hetic.net>
2021-06-04 14:47:56 +0200

    rajout des mots

--- view/script/app.js
+++ view/script/app.js
@@ -4,69 +4,269 @@ new Vue({
         timeBar : 0,
     },
     computed: {
-        JS : function(){
-            if( this.timeBar == 1){
-                return {
-                    bottom : "15%",
-                    left : "20%"
-                }
-            }
-            if( this.timeBar == 2){
-                return {
-                    bottom : "35%",
-                    left : "30%"
-                }
-            }
-            if( this.timeBar == 3){
-                return {
-                    bottom : "35%",
-                    left : "45%"
-                }
-            }
-            if( this.timeBar == 4){
-                return {
-                    bottom : "45%",
-                    left : "60%"
-                }
-            }
-            if( this.timeBar == 5){
-                return {
-                    bottom : "85%",
-                    left : "60%"
-                }
+        // langage
+        // frontc
+        css : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
             }
         },
-        HTML : function(){
-            if( this.timeBar == 1){
-                return {
-                    bottom : "50%",
-                    left : "30%"
-                }
+        c : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
             }
-            if( this.timeBar == 2){
-                return {
-                    bottom : "80%",
-                    left : "50%"
-                }
+        },
+        cpp : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
             }
-            if( this.timeBar == 3){
-                return {
-                    bottom : "90%",
-                    left : "75%"
-                }
+        },
+        // back
+        python : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
             }
-            if( this.timeBar == 4){
-                return {
-                    bottom : "100%",
-                    left : "90%"
-                }
+        },
+        php : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
             }
-            if( this.timeBar == 5){
-                return {
-                    bottom : "100%",
-                    left : "100%"
-                }
+        },
+        java : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
             }
-        }
+        },
+        ruby : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        sql : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        csh : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+
+        // front/back
+        javaScript : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        typeScript : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        objectivec : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        kotlin : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // Frameworks
+        // Front
+        reactjs : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        reactNatif : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        angularJS : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        angular : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        vue : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        bootstrap : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        tailwind : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // back
+        laraval : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        symphony : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        rails : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        django : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        flask : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        cakePHP : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // front/back
+        net : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // serveur
+        apache : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        nginx : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        iis : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // BDD
+        mySQL : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        mongodb : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        SQLServer : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        postgreSQL : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        oracle : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // outils
+        adobeFlash : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        wordpress : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        shopify : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        joomla : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        git : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        nodejs : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        lowCode : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
     }
 })
