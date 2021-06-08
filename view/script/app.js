87df8f11676e9650b3b87b9b79135f559dd1eb63
Gabouil <gabriel.goldbronnn@hetic.net>
2021-06-08 16:10:37 +0200

    amélioration des fonction vue

--- view/script/app.js
+++ view/script/app.js
@@ -4,6 +4,8 @@ const buttonNight = document.getElementById("button_night")
 var nightOn = false
 const legende = document.getElementById("legende")
 var legendeOn = false
+const timeBar = document.getElementById("time_bar")
+
 
 buttonNight.addEventListener('click', function(){
     if(nightOn == false) {
@@ -29,7 +31,7 @@ legende.addEventListener('click', function(){
     }
 })
 
-const canvaTandance = document.getElementById("canvas_tandance")
+const canvaTendance = document.getElementById("canvas_Tendance")
 const canvaEtabli = document.getElementById("canvas_etabli")
 const canvaMourrant = document.getElementById("canvas_mourrant")
 const canvaEmergeant = document.getElementById("canvas_emergeant")
@@ -46,19 +48,22 @@ function elementPosition (a) {
   }
 
 
-var positionTandance = elementPosition(canvaTandance)
+var positionTendance = elementPosition(canvaTendance)
 var positionEtabli = elementPosition(canvaEtabli)
 var positionMourrant = elementPosition(canvaMourrant)
 var positionEmergeant = elementPosition(canvaEmergeant)
 var positionPoints = []
-points.forEach(point => {
-    positionPoints.push(elementPosition(point))
-});
 
+function recupPos() {
+    positionPoints = []
+    points.forEach(point => {
+        positionPoints.push(elementPosition(point))
+    });
+}
 
 function testerCollision(woBox, woPoint ,boxBX, boxAX, boxBY, boxAY, largeurBoxA, largeurBoxB, hauteurBoxA, hauteurBoxB ) {
     if (!(boxBX > boxAX + largeurBoxA || boxBX < boxAX - largeurBoxB || boxBY > boxAY + hauteurBoxA || boxBY < boxAY - hauteurBoxB)) {
-        if(woBox == "Tandance"){
+        if(woBox == "Tendance"){
             points[woPoint].setAttribute('style', 'background-color: #7EA477;')
         }else if(woBox == "Etabli"){
             points[woPoint].setAttribute('style', 'background-color: #55618B;')
@@ -71,287 +76,16 @@ function testerCollision(woBox, woPoint ,boxBX, boxAX, boxBY, boxAY, largeurBoxA
 }
 
 function testColorPoint() {
-    let conteur = 0
+    recupPos()
+    let compteur = 0
     positionPoints.forEach(point => {
-        testerCollision("Tandance", conteur ,point.clientX, positionTandance.clientX, point.clientY, positionTandance.clientY, canvaTandance.offsetWidth, points[conteur].offsetWidth, canvaTandance.offsetHeight, points[conteur].offsetHeight)
-        testerCollision("Etabli", conteur ,point.clientX, positionEtabli.clientX, point.clientY, positionEtabli.clientY, canvaEtabli.offsetWidth, points[conteur].offsetWidth, canvaEtabli.offsetHeight, points[conteur].offsetHeight)
-        testerCollision("Mourrant", conteur ,point.clientX, positionMourrant.clientX, point.clientY, positionMourrant.clientY, canvaMourrant.offsetWidth, points[conteur].offsetWidth, canvaMourrant.offsetHeight, points[conteur].offsetHeight)
-        testerCollision("Emergeant", conteur ,point.clientX, positionEmergeant.clientX, point.clientY, positionEmergeant.clientY, canvaEmergeant.offsetWidth, points[conteur].offsetWidth, canvaEmergeant.offsetHeight, points[conteur].offsetHeight)
-        conteur += 1
+        testerCollision("Tendance", compteur ,point.clientX, positionTendance.clientX, point.clientY, positionTendance.clientY, canvaTendance.offsetWidth, points[compteur].offsetWidth, canvaTendance.offsetHeight, points[compteur].offsetHeight)
+        testerCollision("Etabli", compteur ,point.clientX, positionEtabli.clientX, point.clientY, positionEtabli.clientY, canvaEtabli.offsetWidth, points[compteur].offsetWidth, canvaEtabli.offsetHeight, points[compteur].offsetHeight)
+        testerCollision("Mourrant", compteur ,point.clientX, positionMourrant.clientX, point.clientY, positionMourrant.clientY, canvaMourrant.offsetWidth, points[compteur].offsetWidth, canvaMourrant.offsetHeight, points[compteur].offsetHeight)
+        testerCollision("Emergeant", compteur ,point.clientX, positionEmergeant.clientX, point.clientY, positionEmergeant.clientY, canvaEmergeant.offsetWidth, points[compteur].offsetWidth, canvaEmergeant.offsetHeight, points[compteur].offsetHeight)
+        compteur += 1
     });
 }
 
-testColorPoint()
-
-new Vue({
-    el: "#mon_app",
-    data: {
-        timeBar : 0,
-    },
-    computed: {
-        // langage
-        // frontc
-        css : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        c : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        cpp : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // back
-        python : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        php : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        java : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        ruby : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        sql : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        csh : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-
-        // front/back
-        javaScript : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        typeScript : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        objectivec : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        kotlin : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // Frameworks
-        // Front
-        reactjs : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        reactNatif : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        angularJS : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        angular : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        vue : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        bootstrap : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        tailwind : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // back
-        laraval : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        symphony : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        rails : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        django : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        flask : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        cakePHP : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // front/back
-        net : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // serveur
-        apache : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        nginx : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        iis : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // BDD
-        mySQL : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        mongodb : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        SQLServer : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        postgreSQL : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        oracle : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        // outils
-        adobeFlash : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        wordpress : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        shopify : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        joomla : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        git : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        nodejs : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-        lowCode : function(){
-            return {
-                bottom : "50%",
-                left : "30%"
-            }
-        },
-    }
-})
+testColorPoint();
+points[0].addEventListener("transitionend", testColorPoint);
---
+++ view/script/vueApp.js
@@ -0,0 +1,294 @@
+
+new Vue({
+    el: "#mon_app",
+    data: {
+        timeBar : 1,
+    },
+    computed: {
+        // langage
+        // frontc
+        
+        css : function(){
+            if( this.timeBar == 1){
+                return {
+                    bottom : "15%",
+                    left : "20%"
+                }
+            }
+            if( this.timeBar == 2){
+                return {
+                    bottom : "35%",
+                    left : "30%"
+                }
+            }
+            if( this.timeBar == 3){
+                return {
+                    bottom : "35%",
+                    left : "45%"
+                }
+            }
+            if( this.timeBar == 4){
+                return {
+                    bottom : "45%",
+                    left : "60%"
+                }
+            }
+        },
+        c : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        cpp : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // back
+        python : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        php : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        java : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        ruby : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        sql : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        csh : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+
+        // front/back
+        javaScript : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        typeScript : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        objectivec : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        kotlin : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // Frameworks
+        // Front
+        reactjs : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        reactNatif : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        angularJS : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        angular : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        vue : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        bootstrap : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        tailwind : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // back
+        laraval : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        symphony : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        rails : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        django : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        flask : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        cakePHP : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // front/back
+        net : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // serveur
+        apache : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        nginx : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        iis : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // BDD
+        mySQL : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        mongodb : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        SQLServer : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        postgreSQL : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        oracle : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        // outils
+        adobeFlash : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        wordpress : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        shopify : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        joomla : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        git : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        nodejs : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+        lowCode : function(){
+            return {
+                bottom : "50%",
+                left : "30%"
+            }
+        },
+    }
+})
